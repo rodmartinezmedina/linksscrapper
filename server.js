96a61f3018c55a33fc1427f825ad0e0491961c50
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-10-09 16:14:39 +0200

    .

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ app.get("/", (req, res) => {
   res.send("Hello froms server");
 });
 
-app.get("/websitelinks", async (req, res) => {
+app.get(`/api/webpagelinks`, async (req, res) => {
+  // app.get(`/api/webpagelinks/${inputUrl}`, async (req, res) => {
+  const websiteUrl = req.params.inputUrl;
+
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const pageLinks = [];
-
   await page.goto("https://www.rodmartinezmedina.dev");
 
   // const searchedLinks = await page.$$eval("a", (link) => pageLinks.push(el.value));
